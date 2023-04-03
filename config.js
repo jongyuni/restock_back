0c321028aea3ef2bd94ae4408f86d478c3119263
Jongyun Choi <popi0905@gmail.com>
2023-04-04 00:39:26 +0900

    fix: port 수정

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ function required(key, defaultValue = undefined) {
 
 export const config = {
   host: {
-    port: parseInt(required("HOST_PORT", 3000)),
+    port: parseInt(required("HOST_PORT", 8080)),
   },
 
   db: {
